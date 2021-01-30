da1b55732616d719bedad0169eb7d58b5b97305a
Mohsen <mabbasidesign@yahoo.com>
2021-01-30 14:46:52 +0330

    Add Interfaces

---
+++ Birthday Celebrations/Interfaces/IBirthdate.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Birthday_Celebrations.Interfaces
+{
+    public interface IBirthdate
+    {
+        string BirthDate { get; }
+    }
+}
---
+++ Birthday Celebrations/Interfaces/IIdentifiable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Birthday_Celebrations.Interfaces
+{
+    public interface IIdentifiable
+    {
+        string Id { get; }
+    }
+}
